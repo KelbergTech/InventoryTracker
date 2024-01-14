4f3d5cd8b223d834e355b9981483b612390417dd
Mads Kelberg <35922365+MadsKelberg@users.noreply.github.com>
2024-01-15 00:01:32 +0100

    feat(): First iteration of the BarcodeScanner

--- src/app/inventory/scanner.tsx
+++ src/app/inventory/scanner.tsx
@@ -1,9 +1,10 @@
-import { Text, View } from "react-native";
+import CustomBarCodeScanner from "@components/barcodescanner";
+import { View } from "react-native";
 
 export default function Scanner() {
   return (
     <View>
-      <Text>Scanner Screen</Text>
+      <CustomBarCodeScanner />
     </View>
   );
 }
